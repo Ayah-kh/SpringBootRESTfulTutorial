3b23ea653a6147d318500a56b1489b8dd87d1744
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-06-18 12:22:37 +0300

    First Commit

---
+++ src/main/java/com/tutorial/SpringBootRESTfulTutorial/SpringBootResTfulTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.SpringBootRESTfulTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResTfulTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResTfulTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/SpringBootRESTfulTutorial/SpringBootResTfulTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.SpringBootRESTfulTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResTfulTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
