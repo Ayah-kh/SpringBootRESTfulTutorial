36a6d2836819f36f52d537504269c8292e6fe487
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-06-18 15:29:38 +0300

    First Commit

--- src/main/java/com/tutorial/SpringBootRESTfulTutorial/exception/ResourceNotFountException.java
+++ src/main/java/com/tutorial/SpringBootRESTfulTutorial/exception/ResourceNotFountException.java
@@ -1,4 +1,14 @@
 package com.tutorial.SpringBootRESTfulTutorial.exception;
 
-public class ResourceNotFountException {
+public class ResourceNotFountException extends RuntimeException{
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFountException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s :'%s'",resourceName,fieldName,fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
 }
