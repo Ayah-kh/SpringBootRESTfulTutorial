7d114608388d936ced98e6e8cf77074c5b8aa92a
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-06-18 15:46:24 +0300

    First Commit

--- src/main/java/com/tutorial/SpringBootRESTfulTutorial/service/EmployeeService.java
+++ src/main/java/com/tutorial/SpringBootRESTfulTutorial/service/EmployeeService.java
@@ -1,4 +1,7 @@
 package com.tutorial.SpringBootRESTfulTutorial.service;
 
+import com.tutorial.SpringBootRESTfulTutorial.model.Employee;
+
 public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
 }
---
+++ src/main/java/com/tutorial/SpringBootRESTfulTutorial/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.tutorial.SpringBootRESTfulTutorial.service.impl;
+
+import com.tutorial.SpringBootRESTfulTutorial.model.Employee;
+import com.tutorial.SpringBootRESTfulTutorial.repository.EmployeeRepository;
+import com.tutorial.SpringBootRESTfulTutorial.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
