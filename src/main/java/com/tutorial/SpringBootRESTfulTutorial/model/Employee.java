6608b09653da75c061eec996060454aa841ba78b
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-08-10 11:06:36 +0300

    First Commit

--- src/main/java/com/tutorial/SpringBootRESTfulTutorial/model/Employee.java
+++ src/main/java/com/tutorial/SpringBootRESTfulTutorial/model/Employee.java
@@ -3,18 +3,27 @@ package com.tutorial.SpringBootRESTfulTutorial.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Entity
-@Table(name = "employees")
+@Table(name = "employees",
+uniqueConstraints = {
+        @UniqueConstraint(name ="employeeId_unique",columnNames = "employeeId")
+})
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    private String employeeId;
     @Column(name = "first_name", nullable = false)
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
     @Column(name = "email")
     private String email;
+    private LocalDateTime dateCreated;
+    private LocalDateTime dateUpdated;
 
 }
