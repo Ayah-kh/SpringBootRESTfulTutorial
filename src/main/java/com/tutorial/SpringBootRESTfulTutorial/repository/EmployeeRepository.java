75d69390a2cbfbd503018fe7144e7ee22d6f0bba
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-08-13 13:57:07 +0300

    First Commit

--- src/main/java/com/tutorial/SpringBootRESTfulTutorial/repository/EmployeeRepository.java
+++ src/main/java/com/tutorial/SpringBootRESTfulTutorial/repository/EmployeeRepository.java
@@ -3,5 +3,5 @@ package com.tutorial.SpringBootRESTfulTutorial.repository;
 import com.tutorial.SpringBootRESTfulTutorial.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
